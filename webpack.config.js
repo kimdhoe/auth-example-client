338615b085161973fd5195faad8c5f18efad2063
kimdhoe <kimdhoe1@gmail.com>
2016-09-04 03:54:05 +0900

    Configure production build

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path               = require('path')
+const webpack            = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const PATHS = { app:   path.join(__dirname, 'src')
+              , build: path.join(__dirname, 'dist')
+              }
+
+const config =
+  { context:  __dirname
+  , devtool:  'source-map'
+  , entry:    [ PATHS.app ]
+  , output:   { path:     PATHS.build
+              , filename: 'bundle.js'
+              }
+  , plugins:  [ new webpack.optimize.OccurenceOrderPlugin()
+              , new webpack.optimize.DedupePlugin()
+              , new webpack.DefinePlugin(
+                  { 'process.env.NODE_ENV': JSON.stringify('production')
+                  , 'process.env.API':      JSON.stringify(process.env.API)
+                  }
+                )
+              , new webpack.optimize.UglifyJsPlugin(
+                  { compress: { warnings: false } }
+                )
+              , new CleanWebpackPlugin( [ PATHS.build ]
+                                      , { root: process.cwd() }
+                                      )
+              ]
+  , module:   { loaders: [ { test:    /\.js$/
+                           , include: path.join(__dirname, 'src')
+                           , loaders: [ 'babel' ]
+                           }
+                         ]
+              }
+  , resolve:  { extensions: [ '', '.js' ] }
+  }
+
+module.exports = config
